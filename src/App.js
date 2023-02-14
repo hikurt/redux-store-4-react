7d444e9989b8e95e4378cc23455e7d147b9f113f
hikurt <32220244+hikurt@users.noreply.github.com>
2023-02-14 15:45:43 +0100

    redux-toolkit finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+
 
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      
+      
+      <Counter />
+    </Fragment>
   );
 }
 
